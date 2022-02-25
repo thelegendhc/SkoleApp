4651f8e53a7c0447665c6e5b612749108f8379d2
itzoreomc <42281263+itzoreomc@users.noreply.github.com>
2022-02-25 08:06:25 +0100

    dist

--- dist/_nuxt/static/1645745619/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSkoleApp\u002F_nuxt\u002Fstatic\u002F1645745619",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSkoleApp\u002F",assetsPath:"\u002FSkoleApp\u002F_nuxt\u002F",cdnURL:a}}}}(null));
